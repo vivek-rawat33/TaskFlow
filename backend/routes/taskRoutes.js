a43ed689dd927635ed2fc75d726920bdba4e73b5
vivek <viveksr012@gmail.com>
2026-06-28 17:39:18 +0530

    Add task routes ping

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -7,7 +7,9 @@ import {
 } from "../controllers/taskController.js";
 
 const router = express.Router();
-
+router.get("/ping", (req, res) => {
+  res.json({ message: "Task routes connected" });//
+});
 router.route("/").get(getTasks).post(createTask);
 router.route("/:id").put(updateTask).delete(deleteTask);
 
