3fa81ec86e4382c720bd8a2ee58ca08dfb4f31f0
Vercel <vercel[bot]@users.noreply.github.com>
2026-07-17 05:09:12 +0000

    Install Vercel Web Analytics
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,25 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/Login-page.jsx";
 import SignupPage from "./pages/Signup-page.jsx";
 import Page from "./pages/Dashboard.jsx";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/signup" replace />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
 
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/signin" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signin" element={<LoginPage />} />
 
-      <Route path="/dashboard" element={<Page />} />
+        <Route path="/dashboard" element={<Page />} />
 
-      <Route path="/dashboard/:teamId/*" element={<Page />} />
+        <Route path="/dashboard/:teamId/*" element={<Page />} />
 
-      <Route path="*" element={<Navigate to="/signup" replace />} />
-
+        <Route path="*" element={<Navigate to="/signup" replace />} />
+      </Routes>
       <Analytics />
-    </Routes>
+    </>
   );
 }
 
